9a1c06790167a25fb5272e74a828a7e285dafca3
Tobias Wiklund <tobias@personalcyklar.se>
2026-04-23 09:48:14 +0200

    switch to adapter-static

--- svelte.config.js
+++ svelte.config.js
@@ -1,11 +1,11 @@
 import { vitePreprocess } from "@sveltejs/vite-plugin-svelte";
-import adapter from "@sveltejs/adapter-vercel";
+import adapter from "@sveltejs/adapter-static";
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
   preprocess: vitePreprocess(),
   kit: {
-    adapter: adapter({ runtime: "nodejs22.x" }),
+    adapter: adapter({ fallback: "index.html" }),
   },
 };
 
